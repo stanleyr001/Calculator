c93a1aa83d096e36438d23785a3b38c674ed9473
stanleyr001 <stanleyr001@gmail.com>
2015-08-13 18:47:59 -0700

    Interface change with bugs in layout.

--- app/src/main/java/com/bluetheta/calculator/BasicCalculator.java
+++ app/src/main/java/com/bluetheta/calculator/BasicCalculator.java
@@ -11,20 +11,19 @@
 package com.bluetheta.calculator;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class BasicCalculator extends Activity {
 
-    private LinearLayout buttonLayout;
+    private LinearLayout containerLayout;
+
 
 
     private int buttonDimensions;
@@ -32,8 +31,8 @@ public class BasicCalculator extends Activity {
 
 
 
-    // TextView holds the display view
-    private TextView display;
+    // TextView holds the textViewDisplay view
+    private TextView textViewDisplay;
 
     // Button arrays that store number and operator buttons
     private Button[] numberButtons = new Button[10];
@@ -47,34 +46,55 @@ public class BasicCalculator extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_calculator);
 
-        buttonLayout = (LinearLayout)findViewById(R.id.buttonLayout);
-        buttonDimensions =
-        Log.d("d" , Double.toString(buttonDimensions));
 
 
-        // Create the TextView display object
-        display = (TextView)findViewById(R.id.displayView);
+
+
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+
+
+
+
+
+        containerLayout = (LinearLayout)findViewById(R.id.buttonLayout);
+        buttonDimensions = width / 4;
+
+        // Create the TextView textViewDisplay object
+        textViewDisplay = (TextView)findViewById(R.id.displayView);
 
         // Create numeric Button objects and set their action listeners
         numberButtons[0] = (Button)findViewById(R.id.button_numberZero);
+        numberButtons[0].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[0].setOnClickListener(new NumberButtonClick());
 
         numberButtons[1] = (Button)findViewById(R.id.button_numberOne);
+        numberButtons[1].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[1].setOnClickListener(new NumberButtonClick());
 
         numberButtons[2] = (Button)findViewById(R.id.button_numberTwo);
+        numberButtons[2].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[2].setOnClickListener(new NumberButtonClick());
 
         numberButtons[3] = (Button)findViewById(R.id.button_numberThree);
+        numberButtons[3].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[3].setOnClickListener(new NumberButtonClick());
 
         numberButtons[4] = (Button)findViewById(R.id.button_numberFour);
+        numberButtons[4].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[4].setOnClickListener(new NumberButtonClick());
 
         numberButtons[5] = (Button)findViewById(R.id.button_numberFive);
+        numberButtons[5].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[5].setOnClickListener(new NumberButtonClick());
 
         numberButtons[6] = (Button)findViewById(R.id.button_numberSix);
+        numberButtons[6].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[6].setOnClickListener(new NumberButtonClick());
 
         numberButtons[7] = (Button)findViewById(R.id.button_numberSeven);
@@ -82,9 +102,11 @@ public class BasicCalculator extends Activity {
         numberButtons[7].setOnClickListener(new NumberButtonClick());
 
         numberButtons[8] = (Button)findViewById(R.id.button_numberEight);
+        numberButtons[8].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[8].setOnClickListener(new NumberButtonClick());
 
         numberButtons[9] = (Button)findViewById(R.id.button_numberNine);
+        numberButtons[9].setLayoutParams(new TableRow.LayoutParams(buttonDimensions, buttonDimensions));
         numberButtons[9].setOnClickListener(new NumberButtonClick());
 
 
@@ -123,7 +145,7 @@ public class BasicCalculator extends Activity {
 
 
     /**
-     * OnClickListener class that edits the display TextView when a numeric Button object is
+     * OnClickListener class that edits the textViewDisplay TextView when a numeric Button object is
      * clicked.
      */
 
@@ -132,13 +154,13 @@ public class BasicCalculator extends Activity {
         @Override
         public void onClick(View v) {
             Button numButton = (Button)v;
-            String displayFieldText = display.getText().toString();
+            String displayFieldText = textViewDisplay.getText().toString();
 
             if (displayFieldText.equals("0"))
-                display.setText(numButton.getText());
+                textViewDisplay.setText(numButton.getText());
             else {
                 displayFieldText += numButton.getText();
-                display.setText(displayFieldText);
+                textViewDisplay.setText(displayFieldText);
             }
 
         }
@@ -146,7 +168,7 @@ public class BasicCalculator extends Activity {
     }
 
     /**
-     * OnClickListener class that edits the display TextView when an operator Button object is
+     * OnClickListener class that edits the textViewDisplay TextView when an operator Button object is
      * clicked.
      */
 
@@ -159,50 +181,50 @@ public class BasicCalculator extends Activity {
 
             switch (operatorSymbol) {
                 case "+":
-                    display.setText(display.getText().toString() + " + ");
+                    textViewDisplay.setText(textViewDisplay.getText().toString() + " + ");
                     break;
                 case "-":
-                    display.setText(display.getText().toString() + " - ");
+                    textViewDisplay.setText(textViewDisplay.getText().toString() + " - ");
                     break;
                 case "*":
-                    display.setText(display.getText().toString() + " * ");
+                    textViewDisplay.setText(textViewDisplay.getText().toString() + " * ");
                     break;
                 case "/":
-                    display.setText(display.getText().toString() + " / ");
+                    textViewDisplay.setText(textViewDisplay.getText().toString() + " / ");
                     break;
                 case ".":
-                    display.setText(display.getText().toString()+ " . ");
+                    textViewDisplay.setText(textViewDisplay.getText().toString()+ " . ");
                     break;
                 case "+/-":
                     //TODO Add sign change functionality
                     break;
                 case "(  )":
-                    if(display.getText().toString().equals("0")) {
-                        display.setText("(");
+                    if(textViewDisplay.getText().toString().equals("0")) {
+                        textViewDisplay.setText("(");
                         parenthesisOpen = true;
                     }
                     else if(!parenthesisOpen) {
-                        display.setText(display.getText().toString().concat("("));
+                        textViewDisplay.setText(textViewDisplay.getText().toString().concat("("));
                         parenthesisOpen = true;
                     }
                     else {
-                        display.setText(display.getText().toString().concat(")"));
+                        textViewDisplay.setText(textViewDisplay.getText().toString().concat(")"));
                     }
                     break;
                 case "DEL":
-                    StringBuilder currentDisplay = new StringBuilder(display.getText().toString());
-                    if(display.getText().toString().length() == 1)
-                        display.setText("0");
+                    StringBuilder currentDisplay = new StringBuilder(textViewDisplay.getText().toString());
+                    if(textViewDisplay.getText().toString().length() == 1)
+                        textViewDisplay.setText("0");
                     else {
-                        currentDisplay.deleteCharAt(display.getText().toString().length() - 1);
-                        display.setText(currentDisplay.toString());
+                        currentDisplay.deleteCharAt(textViewDisplay.getText().toString().length() - 1);
+                        textViewDisplay.setText(currentDisplay.toString());
                     }
                     break;
                 case "=":
                     //TODO Add equals operator functionality
                     break;
                 case "C":
-                    display.setText("0");
+                    textViewDisplay.setText("0");
                     break;
             }
 
